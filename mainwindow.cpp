8f9e68aa8620a558a34a0e51a8e74fec0b75a3f1
Siddhartha Mondal <92244610+Witty-31-06@users.noreply.github.com>
2024-10-16 19:10:02 +0530

    Optimised Unoptimised code lmaoo.. 1. Reimplemented bresenham circle and line drawing algorithm for smooth transitions. 2. Created functions for plotting multiple points in pixmap instead of one at a time using QPainter::drawPoints.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,10 +12,10 @@
 #define maxVt 1000
 #define PI 180
 #define GridOffset 1
-#define fps 20
+#define fps 30
 #define initial_eggs 4
 #define prob_of_generation 20
-#define Delay delay(1)
+#define Delay delay(10)
 #define A 10
 #define B 15
 #define baseLine_y -240
@@ -94,36 +94,57 @@ void MainWindow::colorPoint(int x, int y, int r, int g, int b, int penwidth=1) {
     ui->workArea->setPixmap(canvas);
 }
 
-
-
-void MainWindow::markBox(int x,int y,int r,int g,int b)
+QPoint MainWindow::transform(QPoint p)
 {
-    int width = ui->workArea->width();
-    int height = ui->workArea->height();
-    int centerX=width/2;
-    int centerY=height/2;
-    clickedPoint.setX(x);
-    clickedPoint.setY(y);
-    int X=floor((x-centerX)*1.0/GridOffset);
-    int Y=floor((centerY-y)*1.0/GridOffset);
-    int calcX=X*GridOffset+centerX+GridOffset/2.0;
-    int calcY=centerY-Y*GridOffset-GridOffset/2.0;
-    colorPoint(calcX,calcY,r,g,b,GridOffset);
-}
-
-void MainWindow::plotPixel(int x,int y,int r,int g,int b)
-{
-    MainWindow::polygon_points.push_back({x,y});
+    int x = p.x();
+    int y = p.y();
     int centerX=(ui->workArea->width())/2;
     int centerY=ui->workArea->height()/2;
     int newX=x*GridOffset+centerX;
     int newY=centerY-y*GridOffset;
-    markBox(newX,newY,r,g,b);
-    MainWindow::drawn_points.insert({x,y,r,g,b});
+
+    clickedPoint.setX(x);
+    clickedPoint.setY(y);
+    int X=floor((newX-centerX)*1.0/GridOffset);
+    int Y=floor((centerY-newY)*1.0/GridOffset);
+    int calcX=X*GridOffset+centerX+GridOffset/2.0;
+    int calcY=centerY-Y*GridOffset-GridOffset/2.0;
+    return QPoint(calcX, calcY);
+}
+void MainWindow::markBox(int x,int y,int r,int g,int b)
+{
+    QPoint pointToColor = transform(QPoint(x, y));
+    int calcX = pointToColor.x();
+    int calcY = pointToColor.y();
+    colorPoint(calcX,calcY,r,g,b,GridOffset);
 }
 
+void MainWindow::markBoxes(QPolygon points, QColor col, int penwidth=1){
+    QPixmap canvas=ui->workArea->pixmap();
+    QPainter painter(&canvas);
+    QPen pen=QPen(col,penwidth);
+    painter.setPen(pen);
+    painter.drawPoints(points);
+    ui->workArea->setPixmap(canvas);
+}
+void MainWindow::plotPixel(int x,int y,int r,int g,int b)
+{
+
+    markBox(x,y,r,g,b);
+}
+void MainWindow::plotPoints(QPolygon pts, QColor col) {
+
+    QPolygon transformedPoints;
+    for(int i = 0; i<pts.size(); i++)
+    {
+        QPoint p = pts[i];
+        transformedPoints<<transform(p);
+    }
+    markBoxes(transformedPoints, col, GridOffset);
+}
 void MainWindow::gameLoop() {
     // Update the game state and repaint the scene
+    qDebug()<<"Meow";
     static int frame_count=1;
     clear_canvas();
     ui->score->setText(QString::number(score));
@@ -191,7 +212,7 @@ void MainWindow::draw_bressenham_line(int x1, int y1, int x2, int y2,int r,int g
     // Calculate the differences
     int dx = abs(x2 - x1);
     int dy = abs(y2 - y1);
-
+    QPolygon linePoints;
     // Determine if we are stepping in the positive or negative direction
     int sx = (x1 < x2) ? 1 : -1;
     int sy = (y1< y2) ? 1 : -1;
@@ -208,8 +229,7 @@ void MainWindow::draw_bressenham_line(int x1, int y1, int x2, int y2,int r,int g
         for (int i = 0; i <= dx; i++)
         {
             // Convert grid coordinates back to screen coordinates and plot
-            MainWindow::polygon_points.push_back({x,y});
-            MainWindow::plotPixel(x,y,r,g,b);
+            linePoints<<QPoint(x,y);
             // Update p and x, y
             if (p >= 0)
             {
@@ -228,7 +248,7 @@ void MainWindow::draw_bressenham_line(int x1, int y1, int x2, int y2,int r,int g
         {
             // Convert grid coordinates back to screen coordinates and plot
             MainWindow::polygon_points.push_back({x,y});
-            MainWindow::plotPixel(x,y,0,255,0);
+            linePoints<<QPoint(x,y);
 
             // Update p and x, y
             if (p >= 0)
@@ -240,20 +260,23 @@ void MainWindow::draw_bressenham_line(int x1, int y1, int x2, int y2,int r,int g
             y += sy;
         }
     }
+    plotPoints(linePoints, QColor(20,100,220));
 }
 
 
 
 void MainWindow::draw_bressenham_ellipse(int x_center,int y_center,int a,int b)
 {
+    QPolygon points;
+    QColor ellipseColor = QColor(0,0,0);
     int x=0,y=b;
     double d1=b*b-a*a*b+(0.25*a*a);
     while(b*b*x<a*a*y)
     {
-        plotPixel(x_center+x,y_center+y,0,0,0);
-        plotPixel(x_center-x,y_center+y,0,0,0);
-        plotPixel(x_center-x,y_center-y,0,0,0);
-        plotPixel(x_center+x,y_center-y,0,0,0);
+        points<<QPoint(x_center+x,y_center+y)
+               <<QPoint(x_center-x,y_center+y)
+               <<QPoint(x_center-x,y_center-y)
+               <<QPoint(x_center+x,y_center-y);
         if(d1<0)
         {
             d1+=b*b*(1+2*x);
@@ -278,11 +301,20 @@ void MainWindow::draw_bressenham_ellipse(int x_center,int y_center,int a,int b)
             d2+=a*a*(-2*y+3);
         }
         y--;
-        plotPixel(x_center+x,y_center+y,0,0,0);
-        plotPixel(x_center-x,y_center+y,0,0,0);
-        plotPixel(x_center-x,y_center-y,0,0,0);
-        plotPixel(x_center+x,y_center-y,0,0,0);
+
+        points
+            <<QPoint(x_center+x,y_center+y)
+            <<QPoint(x_center-x,y_center+y)
+            <<QPoint(x_center-x,y_center-y)
+            <<QPoint(x_center+x,y_center-y);
     }
+    // qDebug()<<points;
+    // for(int i =  0; i<points.size(); i++)
+    // {
+    //     plotPixel(points[i].x(), points[i].y(), 0,0,0);
+    // }
+    plotPoints(points, ellipseColor);
+
 }
 
 void MainWindow::clear_canvas()
